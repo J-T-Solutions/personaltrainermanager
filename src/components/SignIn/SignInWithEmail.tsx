775552abe7208a4dec1b923bc7d0fbe9eba12494
Wojciech Jeleń <w.jelen20@gmail.com>
2021-06-09 13:55:34 +0200

    Redirecting to home after login

--- src/components/SignIn/SignInWithEmail.tsx
+++ src/components/SignIn/SignInWithEmail.tsx
@@ -1,9 +1,9 @@
-import { useContext } from 'react'
 import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { useHistory } from 'react-router-dom'
 
 import { signInUser } from '../../features/authentication/sessionSlice'
 import { useAppDispatch } from '../../hooks'
+import { Routes } from '../../constants/routes'
 
 interface IFormValues {
   email: string
@@ -28,8 +28,9 @@ export const SignInForm = () => {
         const { email, password } = values
         dispatch(signInUser({ email, password }))
 
-        // setSubmitting(false);
-        // history.push(Routes.Home);
+        // TODO: Move it to saga or thunk
+        setSubmitting(false)
+        history.push(Routes.Home)
       }}
     >
       <Form>
